9e151cfbe296a94ef1ac86a57a2f1fbd058c1b56
hsnbsry <basriakbas@hotmail.com>
2022-04-18 23:38:32 +0200

    18.04

---
+++ 05-reactjs/myapp/src/App.js
@@ -0,0 +1,14 @@
+import HelloWorld from "./components/01-hello-world";
+import Comp from "./components/02-comp";
+
+function App() {
+  return (
+    <div className="App">
+      Bu benim ilk Calismam
+      <HelloWorld/>
+      <Comp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-reactjs/myapp/src/components/02-comp.js
@@ -0,0 +1,9 @@
+const Comp=()=>{
+    return(
+        <div>
+            Bu da farkli bir component
+        </div>
+    )
+}
+
+export default Comp;
